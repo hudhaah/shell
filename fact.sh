934b01a3ddb3395652efd0eba0150807d2dce489
hudhaah <147602136+hudhaah@users.noreply.github.com>
2024-04-24 14:04:42 +0530

    Completed

---
+++ fact.sh
@@ -0,0 +1,22 @@
+#factorial
+
+if [  $# -ne 1 ]
+then
+echo " syntax is <$0><number>"
+exit 1
+fi
+
+fact()
+{
+n=$1
+i=1
+f=1
+while [ $i -le $n ]
+do
+f=`expr $f \*  $i`
+i=`expr $i + 1`
+done
+return $f
+}
+fact $1
+echo "$1! = $?" 
