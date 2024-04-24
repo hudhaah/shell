c855cfa61dd9abdf2915c8c6606d6f0fe2aa09dd
hudhaah <147602136+hudhaah@users.noreply.github.com>
2024-04-24 13:57:46 +0530

    Completed

--- reverse.sh
+++ reverse.sh
@@ -1,25 +1,24 @@
+// reversing file without using tac command
 
-
-#to reverse a file 
+# set -vx
 if [ $# -ne 1 ]
 then
-echo "Syntax is <$0> <filename>"
-exit 1
+ echo "syntax is <$0> <filename>"
+ exit 1
 fi
 if [ -d $1 ]
 then
-echo "$1 is a directory"
-exit 2
+ echo "$1 is a directory"
+ exit 2
 fi
 cp $1 file
 l=`cat file | wc -l`
 revfname="$1.rev"
-rm $revfname 
+rm $revfname
 while [ $l -ge 1 ]
-do
-tail -n 1 file >> $revfname
-l=`expr $l - 1` 
-head -n $l file > temp
-mv temp file
+do 
+ tail -n 1 file >> $revfname
+ l=`expr $l - 1`
+ head -n $l file > temp
+ mv temp file
 done
-
