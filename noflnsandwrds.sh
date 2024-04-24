4e08ac964b006b333cb4543be241b83a71d4f5ed
hudhaah <147602136+hudhaah@users.noreply.github.com>
2024-04-24 13:56:47 +0530

    Completed

---
+++ noflnsandwrds.sh
@@ -0,0 +1,28 @@
+# to count the number of lines and words in a file
+#set -vx
+
+if [ $# -eq 0 ]
+then
+  echo "syntax is <$0> [$filename]"
+  exit 1
+fi
+flag=0
+if [ $# -eq 1 ]
+then
+ term=`tty`
+ exec< $1
+ flag=1
+fi
+while read line
+do
+nol=`expr $nol + 1`
+set $line > /dev/null
+nowds=`expr $nowds + $#`
+done
+echo " number of lines =$nol"
+echo " number of words=$nowds"
+if [ $flag -eq 1 ]
+then
+ exec < $term 
+fi
+exit 0 
